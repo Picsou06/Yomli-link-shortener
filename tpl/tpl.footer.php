c8e52a564f9a647378240e18a4254b4df860df4a
Guillaume Litaudon <guillaume.litaudon@gmail.com>
2019-02-21 20:10:27 +0100

    Deletion of old template

--- tpl/tpl.footer.php
+++
@@ -1,6 +0,0 @@
-</section><footer>
-Codé à l'arrache par <a href="http://warriordudimanche.net">Bronco</a><br />
-Modifié à l'arrache par <a href="https://code.yom.li/">yomli</a>
-</footer>
-</body>
-</html>
